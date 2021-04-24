3ee8ce35c9294e24ed537b9574ae919f4017f898
manoj <manojkry1996@gmail.com>
2021-04-24 23:51:34 +0530

    final submission

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { AngularEditorConfig } from '@kolkov/angular-editor';
 
 @Component({
@@ -12,34 +12,71 @@ export class AppComponent implements OnInit {
   htmlContent = '';
   couponForm: FormGroup;
   rulesArray: FormArray;
-couponStatus=[
-  {value:true,dataValue:"Active"},
-  {value:false,dataValue:"Inactive"}
-]
-  constructor(private fb: FormBuilder) {  }
+
+  isUnlimited: boolean = true;
+  startDate: Date = new Date((new Date()).getFullYear(), (new Date()).getMonth(), (new Date()).getDate());
+  endDate: Date = new Date((new Date()).getFullYear(), (new Date()).getMonth(), (new Date()).getDate());
+
+
+  couponStatus = [
+    { value: true, dataValue: "Active" },
+    { value: false, dataValue: "Inactive" }
+  ]
+
+  constructor(private fb: FormBuilder) { }
   ngOnInit() {
     this.couponForm = this.fb.group({
       coupon_code: ['', Validators.required],
       coupon_type: ['', Validators.required],
       valid_from: ['', Validators.required],
       valid_to: ['', Validators.required],
-      is_active: [undefined, Validators.required],
-      coupon_count: ['', Validators.required],
+      is_active: ['', Validators.required],
       is_unlimited: ['', Validators.required],
       tnc: ['', Validators.required],
       rules: this.fb.array([this.createRule()]),
     });
     console.log(this.couponForm.value);
   }
+
+  onUnlimitedAvailability() {
+    if (this.couponForm.get('coupon_count')) {
+      this.couponForm.removeControl('coupon_count');
+    }
+    this.isUnlimited = true;
+  }
+
+  onLimitedAvailability() {
+    if (!this.couponForm.get('coupon_count')) {
+      this.couponForm.addControl('coupon_count', new FormControl(null, Validators.required));
+    }
+    this.isUnlimited = false;
+  }
+
+
+  myFilter = (d: Date | null): boolean => {
+    const date = (d || new Date());
+    this.startDate = date;
+    // Prevent Saturday and Sunday from being selected.
+    return date >= new Date(
+      (new Date()).getFullYear(), (new Date()).getMonth(), (new Date()).getDate());
+  }
+
+
+  myFilter2 = (d: Date | null): boolean => {
+    const date = (d || new Date());
+    // Prevent Saturday and Sunday from being selected.
+    // console.log(this.startDate)
+    return date >= this.startDate;
+  }
+
+
   createRule(): FormGroup {
     return this.fb.group({
       min_amount: ['', Validators.required],
       max_amount: ['', Validators.required],
       discount_type: ['', Validators.required],
       discount: ['', Validators.required],
-      max_discount: [
-        // {value:'',disabled:(value==''?true:false)}
-      ]
+      max_discount: ['']
 
     })
   }
@@ -125,13 +162,26 @@ couponStatus=[
     this.rulesArray = this.couponForm.get('rules') as FormArray;
     this.rulesArray.push(this.createRule());
   }
-  onDeleteRules(index:number){
+
+  onDeleteRules(index: number) {
     (<FormArray>this.couponForm.get('rules')).removeAt(index);
   }
+
   get rules_controls() { // a getter!
     return (<FormArray>this.couponForm.get('rules')).controls;
   }
-  onSubmit(){
+
+  onSubmit() {
+    (<FormArray>this.couponForm.get('rules')).value.forEach(element => {
+      console.log(element.max_discount)
+      if (element.max_discount == '') {
+        delete element.max_discount;
+      }
+    });
     console.log(this.couponForm.value);
+  
+  }
+  resetForm() {
+    this.couponForm.reset();
   }
 }
